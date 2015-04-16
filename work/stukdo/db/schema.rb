acd053200c410d125ee2c577ccb1454ae3e5f95a
Joey <jos0405@gmail.com>
2015-04-16 12:41:18 +0200

    Added user_id and status

--- work/stukdo/db/schema.rb
+++ work/stukdo/db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150410134159) do
+ActiveRecord::Schema.define(version: 20150416094816) do
 
   create_table "tasks", force: true do |t|
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "state",      default: "to_do"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
